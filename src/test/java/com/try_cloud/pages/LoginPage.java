258416c057578cc1f5b8e9c5cd7be019c3efe148
YuliaB <yulkaby23@gmail.com>
2023-07-15 18:27:43 -0600

    Created tryCloud project

---
+++ src/test/java/com/try_cloud/pages/LoginPage.java
@@ -0,0 +1,34 @@
+package com.try_cloud.pages;
+
+
+import com.try_cloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(id="user")
+    public WebElement userName;
+
+
+
+    @FindBy(id="password")
+    public WebElement password;
+
+    @FindBy(name = "submit-form")
+    public WebElement submit;
+
+
+    public void login(String userNameStr, String passwordStr) {
+        userName.sendKeys(userNameStr);
+        password.sendKeys(passwordStr);
+        submit.click();
+        // verification that we logged
+    }
+
+}
---
+++ src/test/java/com/try_cloud/runners/FailedTestRunner.java
@@ -0,0 +1,15 @@
+package com.try_cloud.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/try_cloud/step_definitions"
+)
+public class FailedTestRunner {
+}
+
+
